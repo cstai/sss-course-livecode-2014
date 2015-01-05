187bac869568f59ab4a3d431ebbc86135d893860
Flurin Egger <flurin@digitpaint.nl>
2015-01-05 10:53:15 +0100

    Add chat example

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ var session = require('express-session');
 var multer = require('multer');
 var mysql = require('mysql');
 var myConnection = require('express-myconnection');
+var socketIo = require('socket.io');
+var http = require('http');
+
 var app = express();
+var server = http.Server(app);
+
+var io = socketIo(server);
+
+io.on("connect", function(socket){
+  console.log("Yay!");
+
+  socket.on("hi", function(succeed){
+    if(succeed == true){
+      console.log("blub!")
+    }
+  })
+
+  socket.on("message", function(message){
+    io.emit("message", message);
+  })
+});
+
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -27,10 +48,12 @@ app.use(myConnection(mysql, {
 var weatherRouter = require('./routes/weather');
 var userRouter = require('./routes/user');
 var filesRouter = require('./routes/files');
+var chatRouter = require('./routes/chat');
 
 app.use("/weer", weatherRouter);
 app.use("/user", userRouter);
 app.use("/files", filesRouter);
+app.use("/chat", chatRouter);
 
 app.get("/:name?", function(req, res){
   var data = {
@@ -56,7 +79,7 @@ app.post("/", function(req, res){
 
 });
 
-app.listen("3000", function(){
+server.listen("3000", function(){
   console.log("really started");
 });
 
